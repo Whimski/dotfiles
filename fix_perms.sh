c3f360e4d55783c2685336cd19df1d44077c4fc8
Tobi Sakatoi <tobizehpanda@gmail.com>
2025-11-03 21:29:13 -0600

    update

--- fix_perms.sh
+++ fix_perms.sh
@@ -4,7 +4,7 @@ TARGET_DIR="/home/tobi/dotfiles"
 
 find "$TARGET_DIR" -type d -exec chmod 755 {} \;
 find "$TARGET_DIR" -type f -exec chmod 644 {} \;
-find "$TARGET_DIR" -type f \( -name "*.sh" -o -name "*.py" -o -name "*.pl" -o -name "*.cgi" -o -name "dotfiles*" \) -exec chmod 755 {} \;
+find "$TARGET_DIR" -type f \( -name "*.sh" -o -name "*.py" -o -name "*.pl" -o -name "*.cgi" -o -name "dotfiles*" -o -name "*-dot" \) -exec chmod 755 {} \;
 
 chmod 600 .ssh/*
 chmod 644 .ssh/*.pub
