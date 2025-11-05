b9069306f507897447c626c2d0364ca7d65aace1
Tobi Sakatoi <tobizehpanda@gmail.com>
2025-11-05 11:13:50 -0600

    emmet

--- .config/nvim/lua/plugins/plugins.lua
+++ .config/nvim/lua/plugins/plugins.lua
@@ -83,5 +83,11 @@ return {
       })
     end,
   },
+  {
+    "olrtg/nvim-emmet",
+    config = function()
+      vim.keymap.set({ "n", "v" }, '<leader>xe', require('nvim-emmet').wrap_with_abbreviation)
+    end,
+  },
 }
 
