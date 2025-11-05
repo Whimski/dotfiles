07e6218ae03eb57173567fed077afdf5d4b08304
TobiZehPanda <tobizehpanda@gmail.com>
2025-11-05 16:46:38 -0600

    emmet

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -19,7 +19,7 @@ return {
     -- Setup Mason
     mason.setup()
     mason_lspconfig.setup({
-      ensure_installed = { "pyright", "lua_ls", "clangd", "bashls" },
+      ensure_installed = { "pyright", "lua_ls", "clangd", "bashls", "emmet_language_server" },
     })
 
     -- Use the new recommended setup (avoids deprecated framework)
