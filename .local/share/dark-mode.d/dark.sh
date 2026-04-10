9539f89b774e3313d707da78d305828c6daadff9
Tobi Sakatoi <tobizehpanda@gmail.com>
2026-04-10 01:16:07 -0500

    noctalia

--- .local/share/dark-mode.d/dark.sh
+++ .local/share/dark-mode.d/dark.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+export HYPRLAND_INSTANCE_SIGNATURE="$(ls /run/user/$UID/hypr/ | head -n 1)"
+
 gsettings set org.gnome.desktop.interface color-scheme prefer-dark
+hyprctl setcursor "Catppuccin Mocha Green" 24
 #gsettings set org.gnome.desktop.interface icon-theme "icon-pack"
--- .local/share/light-mode.d/light.sh
+++ .local/share/light-mode.d/light.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+export HYPRLAND_INSTANCE_SIGNATURE="$(ls /run/user/$UID/hypr/ | head -n 1)"
+
 gsettings set org.gnome.desktop.interface color-scheme prefer-light
+hyprctl setcursor "Catppuccin Mocha Lavender" 24
