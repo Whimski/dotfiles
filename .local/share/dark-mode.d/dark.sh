8a286365e4059b6f10b37979db00376cdaece0c6
Tobi Sakatoi <tobizehpanda@gmail.com>
2026-04-10 10:54:05 -0500

    fix_darkman

--- .local/share/dark-mode.d/dark.sh
+++ .local/share/dark-mode.d/dark.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export HYPRLAND_INSTANCE_SIGNATURE="$(ls /run/user/$UID/hypr/ | head -n 1)"
+export HYPRLAND_INSTANCE_SIGNATURE="$(hyprctl instances | head -n1 | cut -d' ' -f2 | sed 's/://')"
 
 gsettings set org.gnome.desktop.interface color-scheme prefer-dark
 hyprctl setcursor "Catppuccin Mocha Green" 24
--- .local/share/light-mode.d/light.sh
+++ .local/share/light-mode.d/light.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-export HYPRLAND_INSTANCE_SIGNATURE="$(ls /run/user/$UID/hypr/ | head -n 1)"
+
+export HYPRLAND_INSTANCE_SIGNATURE="$(hyprctl instances | head -n1 | cut -d' ' -f2 | sed 's/://')"
 
 gsettings set org.gnome.desktop.interface color-scheme prefer-light
 hyprctl setcursor "Catppuccin Mocha Lavender" 24
